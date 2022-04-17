cd8e4edd3dc3d173eaa2fbc1b6a46640dd1c103e
che30 <che.blancoo58@gmail.com>
2022-04-17 18:14:56 +0100

    Update .env file

--- school/settings.py
+++ school/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['https://schoolappdeploy.herokuapp.com']
 
 AUTH_USER_MODEL = 'account.Account'
 # Application definition
